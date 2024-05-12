2e1425ca5fa3c3d7412326eca7197ad3431fb2ff
phuongboi <kytaithon84@gmail.com>
2024-05-12 23:55:53 +0700

    add attention

--- README.md
+++ README.md
@@ -15,3 +15,16 @@
 * [2] Chen, Yu Fan, et al. "Decentralized non-communicating multiagent collision avoidance with deep reinforcement learning." 2017 IEEE international conference on robotics and automation (ICRA). IEEE, 2017.
 * [3] Everett, Michael, Yu Fan Chen, and Jonathan P. How. "Motion planning among dynamic, decision-making agents with deep reinforcement learning." 2018 IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS). IEEE, 2018.
 * [4] Chen, Changan, et al. "Crowd-robot interaction: Crowd-aware robot navigation with attention-based deep reinforcement learning." 2019 international conference on robotics and automation (ICRA). IEEE, 2019.
+
+2024-05-12 21:58:43, INFO: Using device: cpu
+2024-05-12 21:58:43, INFO: Policy: LSTM-RL w/o pairwise interaction module
+2024-05-12 21:58:43, INFO: human number: 5
+2024-05-12 21:58:43, INFO: Not randomize human's radius and preferred speed
+2024-05-12 21:58:43, INFO: Training simulation: circle_crossing, test simulation: square_crossing
+2024-05-12 21:58:43, INFO: Square width: 10.0, circle width: 4.0
+None
+2024-05-12 21:58:43, INFO: Agent is invisible and has holonomic kinematic constraint
+2024-05-12 22:45:24, INFO: TEST  has success rate: 0.92, collision rate: 0.05, nav time: 11.44, total reward: 0.2382
+2024-05-12 22:45:24, INFO: Frequency of being in danger: 0.15 and average min separate distance in danger: 0.10
+2024-05-12 22:45:24, INFO: Collision cases: 39 48 71 79 113 120 175 210 218 220 236 238 247 258 266 276 287 295 318 358 361 367 404 493
+2024-05-12 22:45:24, INFO: Timeout cases: 2 59 161 167 176 207 228 250 364 382 408 424 492 498
--- crowd_nav/policy/lstm_att.py
+++ crowd_nav/policy/lstm_att.py
@@ -7,16 +7,18 @@ from crowd_nav.policy.multi_human_rl import MultiHumanRL
 
 
 class ValueNetwork1(nn.Module):
-    def __init__(self, input_dim, self_state_dim, mlp_dims, lstm_hidden_dim):
+    def __init__(self, input_dim, self_state_dim, mlp_dims, mlp1_dims, attention_dims, lstm_hidden_dim):
         super().__init__()
         self.self_state_dim = self_state_dim
         self.lstm_hidden_dim = lstm_hidden_dim
+        self.global_state_dim = mlp1_dims[-1]
         self.mlp = mlp(self_state_dim + lstm_hidden_dim, mlp_dims)
         self.lstm = nn.LSTM(input_dim, lstm_hidden_dim, batch_first=True)
 
-    # def rerank_human(self, state):
-    #     self_state = state[:, 0, :self.self_state_dim]
-    #     human_state = state[:, :, self.self_state_dim:]
+        # for attention module
+        self.mlp1 = mlp(13, mlp1_dims, last_relu=True)
+        self.attention = mlp(mlp1_dims[-1] * 2, attention_dims)
+
 
     def forward(self, state):
         """
@@ -28,6 +30,26 @@ class ValueNetwork1(nn.Module):
         size = state.shape
         self_state = state[:, 0, :self.self_state_dim]
         human_state = state[:, :, self.self_state_dim:]
+
+        # add attention module
+        mlp1_output = self.mlp1(state.view((-1, size[2])))
+        global_state = torch.mean(mlp1_output.view(size[0], size[1], -1), 1, keepdim=True)
+        global_state = global_state.expand((size[0], size[1], self.global_state_dim)).\
+            contiguous().view(-1, self.global_state_dim)
+        attention_input = torch.cat([mlp1_output, global_state], dim=1)
+        scores = self.attention(attention_input).view(size[0], size[1], 1).squeeze(dim=2)
+        rerank_human_state = np.zeros(human_state.shape)
+        for i in range(size[0]):
+            score = scores[i, :]
+            ind = torch.argsort(score)
+            rerank_human_state[i, :, :] = human_state[i, ind, :]
+            
+        # masked softmax
+        # weights = softmax(scores, dim=1).unsqueeze(2)
+        # scores_exp = torch.exp(scores) * (scores != 0).float()
+        # weights = (scores_exp / torch.sum(scores_exp, dim=1, keepdim=True)).unsqueeze(2)
+
+
         h0 = torch.zeros(1, size[0], self.lstm_hidden_dim)
         c0 = torch.zeros(1, size[0], self.lstm_hidden_dim)
         output, (hn, cn) = self.lstm(human_state, (h0, c0))
@@ -79,14 +101,16 @@ class LSTM_ATT(MultiHumanRL):
     def configure(self, config):
         self.set_common_parameters(config)
         mlp_dims = [int(x) for x in config.get('lstm_rl', 'mlp2_dims').split(', ')]
+        mlp1_dims = [int(x) for x in config.get('lstm_att', 'mlp1_dims').split(', ')]
         global_state_dim = config.getint('lstm_rl', 'global_state_dim')
         self.with_om = config.getboolean('lstm_rl', 'with_om')
         with_interaction_module = config.getboolean('lstm_rl', 'with_interaction_module')
+        attention_dims = [int(x) for x in config.get('lstm_att', 'attention_dims').split(', ')]
         if with_interaction_module:
             mlp1_dims = [int(x) for x in config.get('lstm_rl', 'mlp1_dims').split(', ')]
             self.model = ValueNetwork2(self.input_dim(), self.self_state_dim, mlp1_dims, mlp_dims, global_state_dim)
         else:
-            self.model = ValueNetwork1(7, self.self_state_dim, mlp_dims, global_state_dim)
+            self.model = ValueNetwork1(7, self.self_state_dim, mlp_dims, mlp1_dims, attention_dims, global_state_dim)
         self.multiagent_training = config.getboolean('lstm_rl', 'multiagent_training')
         logging.info('Policy: {}LSTM-RL {} pairwise interaction module'.format(
             'OM-' if self.with_om else '', 'w/' if with_interaction_module else 'w/o'))
